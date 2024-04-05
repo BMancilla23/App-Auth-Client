9226858fb9469b84554b3148b5f6402b1763d8f0
bmancilla23 <bmancilla119@gmail.com>
2024-04-04 22:13:31 -0500

    Importamos header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { About, Home, Profile, SignIn, SignUp } from "./pages"
+import { Header } from "./components"
 
 export const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
