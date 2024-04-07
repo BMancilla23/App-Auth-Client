30d17ff65f7c0b955cdb06944aacb2c335579b87
bmancilla23 <bmancilla119@gmail.com>
2024-04-07 16:56:56 -0500

    Actualización del componente header y se cambio el perfil a privado

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { About, Home, Profile, SignIn, SignUp } from "./pages"
 import { Header } from "./components"
+import { PrivateRoute } from "./components/PrivateRoute"
 
 export const App = () => {
   return (
     <BrowserRouter>
-    <Header/>
+      <Header />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/sign-in" element={<SignIn/>}/>
-        <Route path="/sign-up" element={<SignUp/>}/>
-        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,10 +2,13 @@ import './Header.css';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 const Header = ({ }) => {
+	const { currentUser } = useSelector((state) => state.user)
 
 	const [open, setOpen] = useState(false)
+
 	const Links = [
 		{
 			name: "Home",
@@ -14,10 +17,6 @@ const Header = ({ }) => {
 		{
 			name: "About",
 			path: "/about"
-		},
-		{
-			name: "Sign In",
-			path: "/sign-in"
 		}
 	]
 
@@ -34,38 +33,63 @@ const Header = ({ }) => {
 						{
 							Links.map((link) => (
 								<li key={link.name}>
-									<NavLink  className= 'mx-4 transitio duration-300'  to={link.path}>{link.name}</NavLink>
+									<NavLink className='mx-4 transitio duration-300' to={link.path}>{link.name}</NavLink>
 								</li>
 							))
 						}
-
+						{currentUser && currentUser.profilePicture ? (
+							<NavLink to='/profile'>
+								<img
+									src={currentUser.profilePicture}
+									alt="Profile"
+									className="rounded-full h-7 w-7 ml-4 object-cover"
+								/>
+							</NavLink>
+						) : (
+							!currentUser && (
+								<li><NavLink className='mx-4 transitio duration-300' to="/sign-in">Sign In</NavLink></li>
+							)
+						)}
 					</ul>
 				</div>
-				<button onClick={toggleMenu} className="md:hidden text-gray-300 hover:text-white focus:outline-none">
-                    {open ? (
-                        <svg viewBox="0 0 24 24" className="h-10 w-10 fill-current">
-                            <path fillRule="evenodd" d="M19.354 4.646a.5.5 0 010 .708l-3.182 3.182 3.182 3.182a.5.5 0 01-.708.708L15.464 9.95l-3.182 3.182a.5.5 0 01-.708-.708l3.182-3.182-3.182-3.182a.5.5 0 01.708-.708L15.464 8.534l3.182-3.182a.5.5 0 01.708 0z"></path>
-                        </svg>
-                    ) : (
-                        <svg viewBox="0 0 24 24" className="h-6 w-6 fill-current">
-                            <path fillRule="evenodd" d="M3 6h18V5H3v1zm0 6h18v-1H3v1zm0 6h18v-1H3v1z"></path>
-                        </svg>
-                    )}
-                </button>
+				<button onClick={toggleMenu} className="md:hidden m-0 text-gray-300 flex hover:text-white focus:outline-none">
+					{open ? (
+						<svg viewBox="0 0 24 24" className="h-10 w-10 fill-current">
+							<path fillRule="evenodd" d="M19.354 4.646a.5.5 0 010 .708l-3.182 3.182 3.182 3.182a.5.5 0 01-.708.708L15.464 9.95l-3.182 3.182a.5.5 0 01-.708-.708l3.182-3.182-3.182-3.182a.5.5 0 01.708-.708L15.464 8.534l3.182-3.182a.5.5 0 01.708 0z"></path>
+						</svg>
+					) : (
+						<svg viewBox="0 0 24 24" className="h-6 w-6 fill-current">
+							<path fillRule="evenodd" d="M3 6h18V5H3v1zm0 6h18v-1H3v1zm0 6h18v-1H3v1z"></path>
+						</svg>
+					)}
+				</button>
 
 			</div>
 			{
 				open && (
 					<div className="md:hidden fixed left-0 flex justify-center items-center w-full h-screen bg-gray-900 ">
-						
+
 						<ul className='flex flex-col items-center text-center'>
 							{
 								Links.map((link) => (
 									<li className='mb-8' key={link.name}>
-										<NavLink  className='mx-4  transition duration-300' onClick={toggleMenu}  to={link.path}>{link.name}</NavLink>
+										<NavLink className='mx-4 transition duration-300' onClick={toggleMenu} to={link.path}>{link.name}</NavLink>
 									</li>
 								))
 							}
+							{currentUser && currentUser.profilePicture ? (
+								<NavLink to='/profile'>
+									<img
+										src={currentUser.profilePicture}
+										alt="Profile"
+										className="rounded-full h-10 w-10 object-cover"
+									/>
+								</NavLink>
+							) : (
+								!currentUser && (
+									<li><NavLink className='mx-4 transitio duration-300' to="/sign-in">Sign In</NavLink></li>
+								)
+							)}
 
 						</ul>
 					</div>
